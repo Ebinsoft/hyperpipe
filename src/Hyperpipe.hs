2a8767ceec83591be575a99eecfeab46dd298880
Jesse <jesseholzman96@gmail.com>
2021-11-05 17:25:31 -0400

    Properly re-export submodules

--- src/Hyperpipe.hs
+++ src/Hyperpipe.hs
@@ -1,4 +1,8 @@
-module Hyperpipe where
+module Hyperpipe
+  ( module Hyperpipe.ConfigParser
+  , module Hyperpipe.EthFrame
+  , module Hyperpipe.StateModel
+  ) where
 
 import Hyperpipe.ConfigParser
 import Hyperpipe.EthFrame
