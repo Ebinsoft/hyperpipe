d266f0b6778323c1bf8afc33bb71c41c4fd112dc
tthurst2 <tylerthurston1@gmail.com>
2021-11-11 18:45:16 -0500

    Change to EthFrame->ByteString->EthFrame test and add Arbitrary instances

--- test/Hyperpipe/Tests/EthFrame.hs
+++ test/Hyperpipe/Tests/EthFrame.hs
@@ -1,24 +1,41 @@
 module Hyperpipe.Tests.EthFrame where
 
-import Test.Tasty
-import Test.Tasty.QuickCheck
+import Data.Binary (decode, encode)
 import Data.ByteString.Lazy (ByteString(..))
 import Data.ByteString.Lazy as BL
-import Data.Binary (encode, decode)
 import Hyperpipe
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Data.Word (Word8)
 
 instance Arbitrary ByteString where
-    arbitrary = return Hyperpipe.packet
+    arbitrary = BL.pack <$> arbitrary
+
+instance Arbitrary MACAddr where
+    arbitrary = MACAddr . BL.pack <$> sequence (Prelude.take 6 $ Prelude.repeat arbitrary)
+
+instance Arbitrary EtherType where
+    arbitrary = EtherType <$> arbitrary
+
+instance Arbitrary VLANTag where
+    arbitrary = VLANTag <$> arbitrary
+
+instance Arbitrary EthFrame where
+    arbitrary = EthFrame <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
 
 tests :: TestTree
 tests = testGroup
     "Hyperpipe.EthFrame"
-    [ testProperty
-        "Decode ByteString to EthFrame and encode back"
-        prop_identity
-    ]
+    [testProperty "Decode ByteString to EthFrame and encode back" prop_identity]
 
-prop_identity :: ByteString -> Bool
-prop_identity bs = 
-    let ef = decode bs :: EthFrame
-    in encode ef == bs
+prop_identity :: EthFrame -> Property
+prop_identity ef@(EthFrame dst src et vl p) =
+    BL.length p >=1 ==>
+    let bs = encode ef :: ByteString
+    in decode bs === ef
+
+
+prop_identity' :: ByteString -> Property
+prop_identity' bs =
+    BL.length bs >= 14 ==> 
+    let ef = decode bs :: EthFrame in encode ef === bs
