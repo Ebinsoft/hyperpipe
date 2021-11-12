37a41c4b6aee160a23c51ff6b162c11ea7cf2061
tthurst2 <tylerthurston1@gmail.com>
2021-11-12 13:47:25 -0500

    Add prop_short test for Test.EthFrame

--- test/Hyperpipe/Tests/EthFrame.hs
+++ test/Hyperpipe/Tests/EthFrame.hs
@@ -1,18 +1,20 @@
 module Hyperpipe.Tests.EthFrame where
 
-import Data.Binary (decode, encode)
+import Data.Binary (decode, decodeOrFail, encode)
+import Data.Binary.Get (ByteOffset)
 import Data.ByteString.Lazy (ByteString(..))
-import Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy as BL
+import Data.Either (isLeft)
+import Data.Word (Word8)
 import Hyperpipe
 import Test.Tasty
 import Test.Tasty.QuickCheck
-import Data.Word (Word8)
 
 instance Arbitrary ByteString where
     arbitrary = BL.pack <$> arbitrary
 
 instance Arbitrary MACAddr where
-    arbitrary = MACAddr . BL.pack <$> sequence (Prelude.take 6 $ Prelude.repeat arbitrary)
+    arbitrary = MACAddr . BL.pack <$> sequence (take 6 $ repeat arbitrary)
 
 instance Arbitrary EtherType where
     arbitrary = EtherType <$> arbitrary
@@ -21,21 +23,32 @@ instance Arbitrary VLANTag where
     arbitrary = VLANTag <$> arbitrary
 
 instance Arbitrary EthFrame where
-    arbitrary = EthFrame <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+    arbitrary =
+        EthFrame
+            <$> arbitrary
+            <*> arbitrary
+            <*> arbitrary
+            <*> arbitrary
+            <*> arbitrary
 
 tests :: TestTree
 tests = testGroup
     "Hyperpipe.EthFrame"
-    [testProperty "Decode ByteString to EthFrame and encode back" prop_identity]
+    [ testProperty
+        "Decoding ByteString to EthFrame and encoding back results in original ByteString"
+        prop_identity
+    , testProperty
+        "decodeOrFailing EthFrame with too few bytes results in a Left"
+        prop_short
+    ]
 
 prop_identity :: EthFrame -> Property
-prop_identity ef@(EthFrame dst src et vl p) =
-    BL.length p >=1 ==>
-    let bs = encode ef :: ByteString
-    in decode bs === ef
+prop_identity ef = let bs = encode ef :: ByteString in decode bs === ef
 
 
-prop_identity' :: ByteString -> Property
-prop_identity' bs =
-    BL.length bs >= 14 ==> 
-    let ef = decode bs :: EthFrame in encode ef === bs
+prop_short :: ByteString -> Property
+prop_short bs = BL.length bs < 14 ==> isLeft
+    (decodeOrFail bs :: Either
+          (ByteString, ByteOffset, String)
+          (ByteString, ByteOffset, EthFrame)
+    )
