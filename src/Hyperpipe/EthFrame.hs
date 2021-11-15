e40115da35101025ef75e8bcdf8d29d016d58461
Jesse <jesseholzman96@gmail.com>
2021-11-15 13:19:24 -0500

    Remove Ord instances (don't actually need them anymore)

--- src/Hyperpipe/EthFrame.hs
+++ src/Hyperpipe/EthFrame.hs
@@ -52,7 +52,7 @@ instance Binary EtherType where
 
 -- | 2-byte VLAN tag
 newtype VLANTag = VLANTag Word16
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq)
 
 -- | A simple Binary instance to get/put a Word16 in big endian, like EtherType
 instance Binary VLANTag where
