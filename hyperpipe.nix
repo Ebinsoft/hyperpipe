cc97e8ed9f9d220b20a9aeec084cfef4661dce11
Jesse <jesseholzman96@gmail.com>
2021-11-15 12:57:15 -0500

    Add mtl dependency

--- hyperpipe.nix
+++ hyperpipe.nix
@@ -1,6 +1,6 @@
 { mkDerivation, base, binary, bytestring, containers, hpack, HsYAML
-, lib, string-interpolate, tasty, tasty-hunit, tasty-quickcheck
-, text
+, lib, mtl, string-interpolate, tasty, tasty-hunit
+, tasty-quickcheck, text
 }:
 mkDerivation {
   pname = "hyperpipe";
@@ -9,15 +9,15 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base binary bytestring containers HsYAML text
+    base binary bytestring containers HsYAML mtl text
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    base binary bytestring containers HsYAML text
+    base binary bytestring containers HsYAML mtl text
   ];
   testHaskellDepends = [
-    base binary bytestring containers HsYAML string-interpolate tasty
-    tasty-hunit tasty-quickcheck text
+    base binary bytestring containers HsYAML mtl string-interpolate
+    tasty tasty-hunit tasty-quickcheck text
   ];
   prePatch = "hpack";
   license = lib.licenses.bsd3;
