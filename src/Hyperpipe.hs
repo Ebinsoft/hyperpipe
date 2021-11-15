2aecd7ba42834aba3f2c51a112b16479f539db2f
Jesse <jesseholzman96@gmail.com>
2021-11-15 16:17:32 -0500

    Add StateMachine to global Hyperpipe namespace

--- src/Hyperpipe.hs
+++ src/Hyperpipe.hs
@@ -2,8 +2,10 @@ module Hyperpipe
   ( module Hyperpipe.ConfigParser
   , module Hyperpipe.EthFrame
   , module Hyperpipe.StateModel
+  , module Hyperpipe.StateMachine
   ) where
 
 import Hyperpipe.ConfigParser
 import Hyperpipe.EthFrame
+import Hyperpipe.StateMachine
 import Hyperpipe.StateModel
