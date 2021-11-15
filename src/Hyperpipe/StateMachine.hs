d4478bd0e5c046b83e0cadbd48458e198f21a4a4
Jesse <jesseholzman96@gmail.com>
2021-11-15 16:22:48 -0500

    Implement functions to produce IO effects from Endpoint data

--- src/Hyperpipe/StateMachine.hs
+++ src/Hyperpipe/StateMachine.hs
@@ -1,10 +1,14 @@
 module Hyperpipe.StateMachine where
 
-import Control.Concurrent (Chan, ThreadId, forkIO, killThread)
+import Control.Concurrent
+  (Chan, ThreadId, forkIO, killThread, readChan, writeChan)
 import Control.Monad.State.Strict
+import Data.Binary (encode)
 import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BL
 import Data.Map.Strict (Map(..))
 import qualified Data.Map.Strict as M
+import Network.Pcap (PcapHandle, nextBS, openLive, sendPacketBS)
 
 import Hyperpipe.EthFrame
 import Hyperpipe.StateModel
@@ -49,6 +53,41 @@ disableEndpoint ep = do
       liftIO $ killThread tid
       put (chn, M.delete name tmap)
 
-
+-- | Infinite loop transferring packets between `Chan` and network interface
 runEndpoint :: Endpoint -> Chan Elem -> IO ()
-runEndpoint = undefined
+runEndpoint ep chn = do
+  let (IfaceName name) = ifaceName ep
+  hnd <- openLive name 10000 True 0
+  let f = runOps $ frameOps ep
+  case trafficDir ep of
+    Input  -> forever $ runInput hnd f chn
+    Output -> forever $ runOutput hnd f chn
+
+-- | Convert a list of `FrameOp`s into a single function over `EthFrame`s doing
+-- all of the ops.
+runOps :: [FrameOp] -> (EthFrame -> EthFrame)
+runOps []       = id
+runOps (o : os) = runOps os . opToFunc o
+ where
+  opToFunc (SetVLAN vt) = setVlan vt
+  opToFunc StripVLAN    = stripVlan
+
+-- | Pull packet from interface handle, apply function, and put into `Chan`.
+runInput :: PcapHandle -> (EthFrame -> EthFrame) -> Chan Elem -> IO ()
+runInput hnd f chn = do
+  (_, bs) <- nextBS hnd
+  let bs' = BL.fromStrict bs
+  elem <- case parseFrame bs' of
+    Left  err -> putStrLn err >> return (Left bs')
+    Right ef  -> return (Right $ f ef)
+  writeChan chn elem
+
+-- | Pull packet from `Chan`, apply function, and write to network interface.
+runOutput :: PcapHandle -> (EthFrame -> EthFrame) -> Chan Elem -> IO ()
+runOutput hnd f chn = do
+  elem <- readChan chn
+  let
+    bs = case elem of
+      Left  bs' -> bs'
+      Right ef  -> encode (f ef)
+  sendPacketBS hnd (BL.toStrict bs)
