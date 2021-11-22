c93781501214ec67905cfaa355d61a89fc4a894e
Jesse <jesseholzman96@gmail.com>
2021-11-22 17:19:16 -0500

    Add basic main function to pull in config file and run it

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+import Control.Concurrent (newChan)
+import Control.Monad (when)
+import Control.Monad.State.Strict (evalStateT)
+import qualified Data.Map.Strict as M
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+import Hyperpipe
+
+
 main :: IO ()
-main = putStrLn "no main yet"
+main = do
+  args <- getArgs
+  when (null args) (putStrLn "gib config file" >> exitFailure)
+
+  res <- parseCfgFile (head args)
+  case res of
+    Left  err   -> putStrLn err >> exitFailure
+    Right model -> do
+      chn <- newChan
+      evalStateT (runWithConfig model) (chn, M.empty)
+
