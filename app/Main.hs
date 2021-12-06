535c6ec418c35302a03d2226d22dd027cb556b93
Jesse <jesseholzman96@gmail.com>
2021-12-06 16:57:15 -0500

    Set line buffering for stdout

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import qualified Data.Map.Strict as M
 import Options.Applicative
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
+import System.IO
 
 import Hyperpipe
 
@@ -17,7 +18,6 @@ data Options = Options
   , optDebug  :: Bool
   }
 
-  
 options :: Parser Options
 options =
   Options
@@ -29,15 +29,15 @@ options =
               "When debug mode is active, each received packet is printed to stdout"
           )
 
-
 optInfo :: ParserInfo Options
 optInfo = info
   (options <**> helper)
   (fullDesc <> header "HYPERPIPE :D")
 
-
 main :: IO ()
 main = do
+  hSetBuffering stdout LineBuffering
+  
   opts <- execParser optInfo
   res  <- parseCfgFile (optConfig opts)
   case res of
