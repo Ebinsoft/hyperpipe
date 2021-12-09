51b37cef3a5d2f417861cbdb2338f200da62908e
Jesse <jesseholzman96@gmail.com>
2021-12-09 12:14:00 -0500

    Add uniagi-chan dependency

--- hyperpipe.nix
+++ hyperpipe.nix
@@ -1,6 +1,6 @@
 { mkDerivation, base, binary, bytestring, containers, hpack, HsYAML
 , lib, mtl, optparse-applicative, pcap, string-interpolate, tasty
-, tasty-hunit, tasty-quickcheck, text
+, tasty-hunit, tasty-quickcheck, text, unagi-chan
 }:
 mkDerivation {
   pname = "hyperpipe";
@@ -9,16 +9,17 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base binary bytestring containers HsYAML mtl pcap text
+    base binary bytestring containers HsYAML mtl pcap text unagi-chan
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
     base binary bytestring containers HsYAML mtl optparse-applicative
-    pcap text
+    pcap text unagi-chan
   ];
   testHaskellDepends = [
     base binary bytestring containers HsYAML mtl pcap
     string-interpolate tasty tasty-hunit tasty-quickcheck text
+    unagi-chan
   ];
   prePatch = "hpack";
   license = lib.licenses.bsd3;
