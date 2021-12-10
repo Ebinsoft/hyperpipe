53ab9b8406fb55897cc6645d0f35d39e32e460db
Jesse <jesseholzman96@gmail.com>
2021-12-10 14:46:38 -0500

    Replace Binary instances with Persist for frame serialization

--- src/Hyperpipe/EthFrame.hs
+++ src/Hyperpipe/EthFrame.hs
@@ -10,11 +10,9 @@
 
 module Hyperpipe.EthFrame where
 
-import Data.Binary (Binary(..), decodeOrFail)
-import Data.Binary.Get
-import Data.Binary.Put (putByteString, putLazyByteString, putWord16be)
-import Data.ByteString.Lazy (ByteString(..))
-import qualified Data.ByteString.Lazy as BL
+import Data.Persist (Persist(..), getByteString, putByteString, getBE, putBE, remaining)
+import Data.ByteString (ByteString(..))
+import qualified Data.ByteString as BS
 import Data.Char (toUpper)
 import Data.List (intercalate)
 import Data.Maybe (isNothing)
@@ -27,37 +25,37 @@ newtype MACAddr = MACAddr ByteString
 
 -- | Custom show instance that writes MAC addresses like: "12:34:45:67:89:AB"
 instance Show MACAddr where
-  show (MACAddr bs) = intercalate ":" (map showByte $ BL.unpack bs)
+  show (MACAddr bs) = intercalate ":" (map showByte $ BS.unpack bs)
    where
     showByte w =
       let hex = map toUpper $ showHex w ""
       in if length hex == 1 then '0' : hex else hex
 
 -- | Custom Binary instance to put/get 6 bytes
-instance Binary MACAddr where
+instance Persist MACAddr where
   get = do
-    m <- getLazyByteString 6
+    m <- getByteString 6
     return $ MACAddr m
 
-  put (MACAddr m) = putLazyByteString m
+  put (MACAddr m) = putByteString m
 
 -- | 2-byte EtherType, specifies the protocol of the frame.
 newtype EtherType = EtherType Word16
   deriving (Show, Eq, Num)
 
 -- | Simple Binary instance to get/put a Word16 in big endian
-instance Binary EtherType where
-  get = EtherType <$> getWord16be
-  put (EtherType e) = putWord16be e
+instance Persist EtherType where
+  get = EtherType <$> getBE
+  put (EtherType e) = putBE e
 
 -- | 2-byte VLAN tag
 newtype VLANTag = VLANTag Word16
   deriving (Show, Eq)
 
 -- | A simple Binary instance to get/put a Word16 in big endian, like EtherType
-instance Binary VLANTag where
-  get = VLANTag <$> getWord16be
-  put (VLANTag vt) = putWord16be vt
+instance Persist VLANTag where
+  get = VLANTag <$> getBE
+  put (VLANTag vt) = putBE vt
 
 -- | The ethernet frame structure,
 data EthFrame = EthFrame
@@ -70,14 +68,14 @@ data EthFrame = EthFrame
   deriving (Show, Eq)
 
 -- | Binary instance to get/put an EthFrame
-instance Binary EthFrame where
+instance Persist EthFrame where
   get = do
     dst     <- get
     src     <- get
     et      <- get
     vt      <- if et == 0x8100 then Just <$> get else return Nothing
     et      <- if isNothing vt then return et else get
-    payload <- getRemainingLazyByteString
+    payload <- remaining >>= getByteString
     return $ EthFrame
       { dstMac       = dst
       , srcMac       = src
@@ -91,15 +89,9 @@ instance Binary EthFrame where
     put src
     case vt of
       Nothing  -> return ()
-      Just vt' -> putWord16be 0x8100 >> put vt'
+      Just vt' -> putBE (0x8100 :: Word16) >> put vt'
     put et
-    putLazyByteString payload
-
--- | Helper function for testing for Failure
-parseFrame :: ByteString -> Either String EthFrame
-parseFrame bs = case decodeOrFail bs of
-  Left  (bs, bo, s ) -> Left s
-  Right (bs, bo, et) -> Right et
+    putByteString payload
 
 -- | Assigns a given VLAN tag to a frame (overwriting any existing tag)
 setVlan :: VLANTag -> EthFrame -> EthFrame
