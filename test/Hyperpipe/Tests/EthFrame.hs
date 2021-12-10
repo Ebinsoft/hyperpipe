371c723eba7ffb0c8c4079ed688b0464423f654d
Jesse <jesseholzman96@gmail.com>
2021-12-10 15:15:01 -0500

    Fix broken EthFrame tests (replace Binary usage with Persist)

--- test/Hyperpipe/Tests/EthFrame.hs
+++ test/Hyperpipe/Tests/EthFrame.hs
@@ -1,50 +1,55 @@
 module Hyperpipe.Tests.EthFrame where
 
-import Data.Binary (decode, decodeOrFail, encode)
-import Data.Binary.Get (ByteOffset)
-import Data.ByteString.Lazy (ByteString(..))
-import qualified Data.ByteString.Lazy as BL
+import Control.Monad (replicateM)
+import Data.ByteString (ByteString(..))
+import qualified Data.ByteString as BS
 import Data.Either (isLeft)
+import Data.Persist (decode, encode)
 import Data.Word (Word8)
 import Hyperpipe
 import Test.Tasty
 import Test.Tasty.QuickCheck
 
 instance Arbitrary ByteString where
-    arbitrary = BL.pack <$> arbitrary
+  arbitrary = BS.pack <$> arbitrary
 
 instance Arbitrary MACAddr where
-    arbitrary = MACAddr . BL.pack <$> sequence (take 6 $ repeat arbitrary)
+  arbitrary = MACAddr . BS.pack <$> replicateM 6 arbitrary
 
 instance Arbitrary EtherType where
-    arbitrary = EtherType <$> arbitrary
+  arbitrary = EtherType <$> arbitrary
 
 instance Arbitrary VLANTag where
-    arbitrary = VLANTag <$> arbitrary
+  arbitrary = VLANTag <$> arbitrary
 
 instance Arbitrary EthFrame where
-    arbitrary =
-        EthFrame
-            <$> arbitrary
-            <*> arbitrary
-            <*> arbitrary
-            <*> arbitrary
-            <*> arbitrary
+  arbitrary =
+    EthFrame
+      <$> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
 
 tests :: TestTree
 tests = testGroup
-    "Hyperpipe.EthFrame"
-    [ testProperty
-        "Decoding ByteString to EthFrame and encoding back results in original ByteString"
-        prop_identity
-    , testProperty
-        "decodeOrFailing EthFrame with too few bytes results in a Left"
-        prop_failOnTooFewBytes
-    ]
+  "Hyperpipe.EthFrame"
+  [ testProperty
+    "Decoding ByteString to EthFrame and encoding back results in original ByteString"
+    prop_identity
+  , testProperty
+    "decodeOrFailing EthFrame with too few bytes results in a Left"
+    prop_failOnTooFewBytes
+  ]
 
 prop_identity :: EthFrame -> Property
-prop_identity ef = let bs = encode ef :: ByteString in decode bs === ef
+prop_identity ef = let bs = encode ef :: ByteString in decodeOrErr bs === ef
 
 prop_failOnTooFewBytes :: ByteString -> Property
 prop_failOnTooFewBytes bs =
-    BL.length bs < 14 ==> isLeft (parseFrame bs :: Either String EthFrame)
+  BS.length bs < 14 ==> isLeft (decode bs :: Either String EthFrame)
+
+decodeOrErr :: ByteString -> EthFrame
+decodeOrErr bs = case decode bs of
+  Left  err -> error err
+  Right ef  -> ef
