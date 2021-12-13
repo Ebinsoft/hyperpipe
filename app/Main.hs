ee044e863390c27b0e677e5c580eb6b03214ec0a
Jesse <jesseholzman96@gmail.com>
2021-12-13 16:48:03 -0500

    Add warning if runtime threads exceed physical core count.

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,15 @@ import Control.Concurrent.Chan.Unagi.Bounded (newChan)
 import Control.Monad (when)
 import Control.Monad.Reader (runReaderT)
 import Control.Monad.State.Strict (evalStateT)
+import Data.Foldable (asum)
+import Data.List (find, isPrefixOf)
 import qualified Data.Map.Strict as M
+import GHC.Conc (getNumCapabilities)
 import Options.Applicative
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO
+import Text.Read (readMaybe)
 
 import Hyperpipe
 
@@ -55,9 +59,40 @@ options =
 optInfo :: ParserInfo Options
 optInfo = info (options <**> helper) (fullDesc <> header "HYPERPIPE :D")
 
+-- | Parse @/proc/cpuinfo@ for the number of physical CPU cores on the system
+getNumCores :: IO Int
+getNumCores = do
+  cpuinfo <- readFile "/proc/cpuinfo"
+  case find ("cpu cores" `isPrefixOf`) (lines cpuinfo) of
+    Nothing -> do
+      putStrLn "Unable to locate CPU core count in /proc/cpuinfo"
+      return 0
+    Just coreinfo -> case asum $ readMaybe <$> words coreinfo of
+      Nothing -> do
+        putStrLn "Unable to locate CPU core count in /proc/cpuinfo"
+        return 0
+      Just n -> return n
+
+
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
+
+  -- Warn user if more RTS threads than physical cores on system.
+  -- Because of the large amount of resource contention between the
+  -- worker threads, hyperthreading tends to really hurt performance.
+  ncores   <- getNumCores
+  nthreads <- getNumCapabilities
+  when (ncores > 0 && nthreads > ncores) $ putStrLn
+    (  "WARNING: number of runtime threads ("
+    ++ show nthreads
+    ++ ") exceeds the number of physical CPU cores ("
+    ++ show ncores
+    ++ ").  "
+    ++ "For best performance set '+RTS -N"
+    ++ show ncores
+    ++ " -RTS'"
+    )
 
   opts <- execParser optInfo
   res  <- parseCfgFile (optConfig opts)
