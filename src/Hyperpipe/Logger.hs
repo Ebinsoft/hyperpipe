c22c0db6fcadb6c9908c79c732e38f959779997f
Jesse <jesseholzman96@gmail.com>
2021-12-22 16:11:42 -0500

    Add timestamps and color to log messages

--- src/Hyperpipe/Logger.hs
+++ src/Hyperpipe/Logger.hs
@@ -17,8 +17,11 @@ import Control.Concurrent (forkIO)
 import Control.Concurrent.Chan.Unagi
   (InChan, OutChan, newChan, readChan, writeChan)
 import Control.Monad (when)
-import Control.Monad.Reader (ReaderT, runReaderT, ask)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (ReaderT, ask, runReaderT)
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import Data.Time.Format (defaultTimeLocale, formatTime)
+import Text.Printf (printf, PrintfArg(..))
 
 data Logger = Logger
   { logChan  :: InChan LogMsg
@@ -32,7 +35,13 @@ data LogLevel
   | ERROR
   deriving (Eq, Ord, Show)
 
-type LogMsg = (LogLevel, String)
+instance PrintfArg LogLevel where
+  formatArg DEBUG = formatArg "\x1b[37mDEBUG\x1b[0m"
+  formatArg INFO  = formatArg "\x1b[36mINFO \x1b[0m"
+  formatArg WARN  = formatArg "\x1b[33mWARN \x1b[0m"
+  formatArg ERROR = formatArg "\x1b[31mERROR\x1b[0m"
+
+type LogMsg = (UTCTime, LogLevel, String)
 
 class (Monad m) => HasLogger m where
   getLogger :: m Logger
@@ -46,27 +55,24 @@ makeLogger lvl = do
   forkIO $ runLogWorker outChn
   return $ Logger inChn lvl
 
-prefix :: LogLevel -> String
-prefix DEBUG = "[DEBUG] "
-prefix INFO  = "[INFO]  "
-prefix WARN  = "[WARN]  "
-prefix ERROR = "[ERROR] "
-
 runLogWorker :: OutChan LogMsg -> IO ()
 runLogWorker chn = loop
  where
   loop = do
-    (lvl, msg) <- readChan chn
-    putStrLn $ prefix lvl ++ msg
+    (time, lvl, msg) <- readChan chn
+    let timeS = formatTime defaultTimeLocale "%F %X" time
+    let log = printf "[%s] %s | %s" timeS lvl msg
+    putStrLn log
     loop
-  
+
 withLogger :: Logger -> ReaderT Logger m () -> m ()
 withLogger l m = runReaderT m l
-  
+
 logWithLevel :: (HasLogger m, MonadIO m) => LogLevel -> String -> m ()
 logWithLevel lvl msg = do
   Logger {..} <- getLogger
-  when (lvl >= minLevel) (liftIO $ writeChan logChan (lvl, msg))
+  time        <- liftIO getCurrentTime
+  when (lvl >= minLevel) (liftIO $ writeChan logChan (time, lvl, msg))
 
 logDebug :: (HasLogger m, MonadIO m) => String -> m ()
 logDebug = logWithLevel DEBUG
