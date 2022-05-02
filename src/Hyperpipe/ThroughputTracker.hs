ea6dd100543340b350ffc59a9e1bd3445013f6ae
Jesse <jesseholzman96@gmail.com>
2022-05-02 16:26:06 -0400

    Replace list with Seq for better performance

--- src/Hyperpipe/ThroughputTracker.hs
+++ src/Hyperpipe/ThroughputTracker.hs
@@ -2,18 +2,20 @@ module Hyperpipe.ThroughputTracker where
 
 import Data.Map (Map(..))
 import qualified Data.Map as M
+import Data.Sequence (Seq(..), ViewR(..), (|>))
+import qualified Data.Sequence as S
 import Data.Time.Clock (UTCTime, diffUTCTime, getCurrentTime)
 
 type IfaceName = String
 type PacketSize = Int
 
-type ThroughputTracker = Map IfaceName [(UTCTime, PacketSize)]
+type ThroughputTracker = Map IfaceName (Seq (UTCTime, PacketSize))
 
 
 pruneOldMetrics :: ThroughputTracker -> IO ThroughputTracker
 pruneOldMetrics tt = do
   time <- getCurrentTime
-  return $ filter (\(pTime, pSize) -> diffUTCTime time pTime < 1.0) <$> tt
+  return $ S.dropWhileL (\(pTime, pSize) -> diffUTCTime time pTime > 1.0) <$> tt
 
 addMetric
   :: ThroughputTracker
@@ -21,8 +23,14 @@ addMetric
   -> (UTCTime, PacketSize)
   -> IO ThroughputTracker
 addMetric tt iface (ts, size) = do
-  let metrics = M.findWithDefault [] iface tt
-  pruneOldMetrics (M.insert iface ((ts, size) : metrics) tt)
+  let metrics = M.findWithDefault Empty iface tt
+  pruneOldMetrics (M.insert iface (insertSorted (ts, size) metrics) tt)
+ where
+  insertSorted (newTime, newSize) elems = case S.viewr elems of
+    EmptyR                       -> elems |> (newTime, newSize)
+    (xs :> (lastTime, lastSize)) -> if newTime < lastTime
+      then insertSorted (newTime, newSize) xs |> (lastTime, lastSize)
+      else elems |> (newTime, newSize)
 
 getThroughput :: ThroughputTracker -> IO (Map IfaceName (Int, Int))
 getThroughput tt = do
