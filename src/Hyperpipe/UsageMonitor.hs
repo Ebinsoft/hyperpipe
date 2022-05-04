08d86011fa07375050ef02d88c4fa5f1eaf594fe
Jesse <jesseholzman96@gmail.com>
2022-05-04 15:58:18 -0400

    Simplify addMetric function

--- src/Hyperpipe/UsageMonitor.hs
+++ src/Hyperpipe/UsageMonitor.hs
@@ -21,27 +21,27 @@ type UsageMonitor = Map IfaceName (Seq (UTCTime, Int))
 pruneOldMetrics :: UsageMonitor -> IO UsageMonitor
 pruneOldMetrics monitor = do
   time <- getCurrentTime
-  -- seq should always be ordered old to new so only need to drop from left size
+  -- seq will always be ordered old to new so only need to drop from left side
   return $ S.dropWhileL (\i -> diffUTCTime time (fst i) > 1.0) <$> monitor
 
 -- | Create a data point for a given network interface, using the timestamp and
 -- size of one packet sent/received on that interface.
 addMetric :: UsageMonitor -> IfaceName -> (UTCTime, Int) -> IO UsageMonitor
-addMetric monitor iface (time, size) = do
+addMetric monitor iface pktInfo = do
   let metrics = M.findWithDefault Empty iface monitor
-  pruneOldMetrics (M.insert iface (insertSorted (time, size) metrics) monitor)
+  pruneOldMetrics (M.insert iface (insertSorted pktInfo metrics) monitor)
  where
-  insertSorted (newTime, newSize) elems = case S.viewr elems of
-    EmptyR                       -> elems |> (newTime, newSize)
-    (xs :> (lastTime, lastSize)) -> if newTime < lastTime
-      then insertSorted (newTime, newSize) xs |> (lastTime, lastSize)
-      else elems |> (newTime, newSize)
+  time = fst -- alias for readability in function below
+  insertSorted x ySeq = case S.viewr ySeq of
+    EmptyR -> S.singleton x
+    (ys :> y) ->
+      if (time x) < (time y) then insertSorted x ys |> y else ySeq |> x
 
 -- | Get the current throughput information for all interfaces as a map from
 -- interface name to a tuple containing the number of packets and total number
 -- of bytes to pass through that interface within the last second.
 getThroughput :: UsageMonitor -> IO (Map String (Int, Int))
 getThroughput monitor = unwrapKeys . calcThroughput <$> pruneOldMetrics monitor
-  where
-    calcThroughput = fmap (\ms -> (length ms, sum (snd <$> ms)))
-    unwrapKeys = M.mapKeys (\(IfaceName n) -> n)
+ where
+  calcThroughput = fmap (\ms -> (length ms, sum (snd <$> ms)))
+  unwrapKeys     = M.mapKeys (\(IfaceName n) -> n)
